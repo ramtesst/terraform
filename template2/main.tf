b8a156bdbef46c30da5cee7e727e3cc9a978a325
Rambabu Duddukuri <duddukurir@vmware.com>
2020-05-11 19:17:33 -0700

    Adding template2 tags and variables

--- template2/main.tf
+++ template2/main.tf
@@ -1,9 +1,32 @@
-resource "aws_instance" "ubuntu" {
+provider "aws" {
+    region="us-east-1"
+}
+
+variable "instance_type" {
+  description = "AWS instance type"
+  default     = "t2.micro"
+}
+
+variable "department" {
+  description = "Department tag"
+}
+
+resource "aws_instance" "machine1" {
+    ami           = "ami-04b9e92b5572fa0d1"
+    instance_type = var.instance_type
+    availability_zone = "us-east-1a"
+
+    tags = {
+        department = var.department
+    }
+}
+
+resource "aws_instance" "machine2" {
     ami           = "ami-04b9e92b5572fa0d1"
     instance_type = "t2.micro"
     availability_zone = "us-east-1a"
-}
 
-provider "aws" {
-    region="us-east-1"
+    tags = {
+        department = var.department
+    }
 }
