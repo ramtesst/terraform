7c26e53e05283d26d0a16f929ed1e7c953b6a236
Rambabu Duddukuri <duddukurir@vmware.com>
2020-08-15 10:53:59 -0700

    Adding output to namespace

--- calatrava/main.tf
+++ calatrava/main.tf
@@ -6,10 +6,6 @@ variable "nimbus_nsname" {
 
 }
 
-variable "guest_cluster_name" {
-
-}
-
 variable "storageclass" {
   default = "wdc-08-vc04c01-wcp-mgmt"
 }
@@ -23,28 +19,6 @@ resource "pacific_nimbus_namespace" "ns" {
    nimbus_config_file = "http://sc-dbc1212.eng.vmware.com/tommyl/mts-git/nimbus-configs/config/staging/wcp.json"
 }
 
-resource "pacific_guestcluster" "gc" {
-   cluster_name = "${var.guest_cluster_name}"
-   namespace = "${pacific_nimbus_namespace.ns.namespace}"
-   input_kubeconfig = "${pacific_nimbus_namespace.ns.kubeconfig}"
-   version = "v1.16"
-   network_servicedomain = "cluster.local"
-   topology_controlplane_class = "best-effort-small"
-   topology_workers_class = "best-effort-small"
-   topology_workers_count = 1
-   topology_controlplane_storageclass = "${var.storageclass}"
-   topology_workers_storageclass = "${var.storageclass}"
-   storage_defaultclass = "${var.storageclass}"
-   tags = {
-      foo = "bar-1"
-      bar = "foo-1"
-   }
-}
-
 output "sv_kubeconfig" {
   value = "${pacific_nimbus_namespace.ns.kubeconfig}"
-}
-
-output "gc_kubeconfig" {
-  value = "${pacific_guestcluster.gc.kubeconfig}"
 }
