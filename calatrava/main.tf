65ddc4e9fd9bad10b0fa90e2946fbf48129fb50a
Rambabu Duddukuri <duddukurir@vmware.com>
2020-08-15 16:20:35 -0700

    Adding kafka and postgres

--- calatrava/main.tf
+++ calatrava/main.tf
@@ -6,6 +6,18 @@ variable "nimbus_nsname" {
 
 }
 
+variable "guest_cluster_name" {
+
+}
+
+variable "kafka_cluster_name" {
+
+}
+
+variable "postgres_cluster_name" {
+
+}
+
 variable "storageclass" {
   default = "wdc-08-vc04c01-wcp-mgmt"
 }
@@ -19,6 +31,50 @@ resource "pacific_nimbus_namespace" "ns" {
    nimbus_config_file = "http://sc-dbc1212.eng.vmware.com/tommyl/mts-git/nimbus-configs/config/staging/wcp.json"
 }
 
+resource "pacific_guestcluster" "gc" {
+   cluster_name = "${var.guest_cluster_name}"
+   namespace = "${pacific_nimbus_namespace.ns.namespace}"
+   input_kubeconfig = "${pacific_nimbus_namespace.ns.kubeconfig}"
+   version = "v1.16"
+   network_servicedomain = "cluster.local"
+   topology_controlplane_class = "best-effort-small"
+   topology_workers_class = "best-effort-small"
+   topology_workers_count = 1
+   topology_controlplane_storageclass = "${var.storageclass}"
+   topology_workers_storageclass = "${var.storageclass}"
+   storage_defaultclass = "${var.storageclass}"
+   tags = {
+      foo = "bar-1"
+      bar = "foo-1"
+   }
+}
+
+resource "pacific_kafka_kafka" "tfkafka6" {
+   cluster_name = "${var.kafka_cluster_name}"
+   number_of_broker_nodes = 1
+   namespace = "${pacific_nimbus_namespace.ns.namespace}"
+   input_kubeconfig = "${pacific_nimbus_namespace.ns.kubeconfig}"
+   kafka_version = "2.4.0"
+   broker_storageclass = "${var.storageclass}"
+   broker_volume_size = "100Gi"
+   zookeeper_storageclass = "${var.storageclass}"
+   zookeeper_volume_size = "100Gi"
+   secure = true
+}
+
+resource "pacific_zalando_postgres" "tfpg5" {
+   cluster_name = "${var.postgres_cluster_name}"
+   namespace = "${pacific_nimbus_namespace.ns.namespace}"
+   input_kubeconfig = "${pacific_nimbus_namespace.ns.kubeconfig}"
+   postgres_version = "11"
+   storageclass = "${var.storageclass}"
+   volume_size = "100Gi"
+}
+
 output "sv_kubeconfig" {
   value = "${pacific_nimbus_namespace.ns.kubeconfig}"
+}
+
+output "gc_kubeconfig" {
+  value = "${pacific_guestcluster.gc.kubeconfig}"
 }
