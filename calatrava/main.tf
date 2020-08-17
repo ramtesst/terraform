90072cd512f458679ac44d9ef67ba39dce3b073a
Rambabu Duddukuri <duddukurir@vmware.com>
2020-08-16 22:48:07 -0700

    Adding minio

--- calatrava/main.tf
+++ calatrava/main.tf
@@ -6,20 +6,16 @@ variable "nimbus_nsname" {
 
 }
 
-variable "guest_cluster_name" {
-
-}
-
-variable "postgres_cluster_name" {
-
-}
-
 variable "nimbus_server" {
   default = "wdc-08-vc07"
 }
 
 variable "storageclass" {
   default = "wdc-08-vc07c01-wcp-mgmt"
+}
+
+variable "minio_cluster_name" {
+
 }
 
 # Keep the nimbus server/config/ip values, they are fine for you to use
@@ -31,37 +27,25 @@ resource "pacific_nimbus_namespace" "ns" {
    nimbus_config_file = "http://sc-dbc1212.eng.vmware.com/tommyl/mts-git/nimbus-configs/config/staging/wcp.json"
 }
 
-resource "pacific_guestcluster" "gc" {
-   cluster_name = "${var.guest_cluster_name}"
+resource "pacific_minio" "tfminio1" {
+   cluster_name = "${var.minio_cluster_name}"
    namespace = "${pacific_nimbus_namespace.ns.namespace}"
    input_kubeconfig = "${pacific_nimbus_namespace.ns.kubeconfig}"
-   version = "v1.16"
-   network_servicedomain = "cluster.local"
-   topology_controlplane_class = "best-effort-small"
-   topology_workers_class = "best-effort-small"
-   topology_workers_count = 1
-   topology_controlplane_storageclass = "${var.storageclass}"
-   topology_workers_storageclass = "${var.storageclass}"
-   storage_defaultclass = "${var.storageclass}"
+   storageclass = "${var.storageclass}"
+   volume_size = "100Gi"
+   access_key = "minio"
+   secret_key = "minio123"
+   replicas = 4
+
+   # Encryption
+   # CpU/Mem size
+
    tags = {
       foo = "bar-1"
       bar = "foo-1"
    }
 }
 
-resource "pacific_zalando_postgres" "tfpg5" {
-   cluster_name = "${var.postgres_cluster_name}"
-   namespace = "${pacific_nimbus_namespace.ns.namespace}"
-   input_kubeconfig = "${pacific_nimbus_namespace.ns.kubeconfig}"
-   postgres_version = "11"
-   storageclass = "${var.storageclass}"
-   volume_size = "100Gi"
-}
-
 output "sv_kubeconfig" {
   value = "${pacific_nimbus_namespace.ns.kubeconfig}"
-}
-
-output "gc_kubeconfig" {
-  value = "${pacific_guestcluster.gc.kubeconfig}"
 }
