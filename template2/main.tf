fad9b65bbe92bb71cbfba953790cad1892ec5567
Rambabu Duddukuri <duddukurir@vmware.com>
2020-09-10 21:52:42 -0700

    Updating template2

--- template2/main.tf
+++ template2/main.tf
@@ -30,3 +30,15 @@ resource "aws_instance" "machine2" {
         department = var.department
     }
 }
+
+output "instance_ip_addr" {
+  value = aws_instance.machine1.*.public_ip
+}
+
+output "instance_ip_addr_private" {
+  value = aws_instance.machine1.*.private_ip
+}
+
+output "instance_state" {
+  value = aws_instance.machine1.instance_state
+}
