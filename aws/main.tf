df658f5deceeac52f81be06df180733255cadb54
Rambabu Duddukuri <duddukurir@vmware.com>
2020-09-17 00:36:28 -0700

    Adding provisioner

--- aws/main.tf
+++ aws/main.tf
@@ -16,7 +16,7 @@ variable "private_key" {
 }
 
 resource "aws_instance" "machine1" {
-    ami           = "ami-04b9e92b5572fa0d1"
+    ami           = "ami-693d3483"
     instance_type = "t2.micro"
     availability_zone = "us-east-1a"
 
@@ -30,6 +30,24 @@ resource "aws_instance" "machine1" {
       private_key = var.private_key
       host = self.public_ip
       # The connection will use the local SSH agent for authentication.
+    }
+
+    provisioner "remote-exec" {
+      inline = [
+        "sudo yum update -y",
+        "sudo yum install epel-release -y",
+        "sudo yum install httpd -y",
+        "sudo yum install ansible -y",
+        "sudo /usr/sbin/service httpd start",
+        "sudo yum install php -y",
+        "sudo yum install php-mysql -y",
+        "sudo /usr/sbin/chkconfig httpd on",
+        "sudo yum install firewalld -y",
+  #      "sudo service firewalld start && sudo firewall-cmd --zone=public --add-port=80/tcp --permanent && sudo firewall-cmd --zone=public --add-port=22/tcp --permanent && sudo firewall-cmd --reload",
+        "cd /tmp/",
+        "sudo curl -O https://raw.githubusercontent.com/vmeoc/Tito/master/asset/Deployment/Ansible/tito-fe-git.yml",
+        "sudo ansible-playbook tito-fe-git.yml -e git_url=https://github.com/vmeoc/Tito/ -e tito_release=V1.9",
+      ]
     }
 }
 
