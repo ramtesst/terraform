64fdf0267f9c923ac619b954aaa0f8e7dbae26a7
Rambabu Duddukuri <duddukurir@vmware.com>
2020-10-16 09:49:06 -0700

    Updating restapi

--- restapi/main.tf
+++ restapi/main.tf
@@ -1,3 +1,19 @@
+provider "aws" {
+    region="us-east-1"
+}
+
+variable "instance_type" {
+  description = "AWS instance type"
+  default     = "t2.micro"
+}
+
+
+resource "aws_instance" "machine1" {
+    ami           = "ami-04b9e92b5572fa0d1"
+    instance_type = "t2.micro"
+    availability_zone = "us-east-1a"
+}
+
 data "restapi2" "weather" {
   uri          = "https://api.weather.gov/gridpoints/PHI/63,115/forecast"
   method       = "GET"
