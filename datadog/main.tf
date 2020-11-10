ed12424b249aa54aa9a01c268942a4616bd751b6
Rambabu Duddukuri <duddukurir@vmware.com>
2020-11-09 21:30:39 -0800

    DataDog variables

--- datadog/main.tf
+++ datadog/main.tf
@@ -1,3 +1,12 @@
+variable "datadog_api_key" {
+  description = "Datadog API Key"
+}
+
+variable "datadog_app_key" {
+  description = "Datadog App Key"
+}
+
+
 provider "datadog" {
   api_key = "${var.datadog_api_key}"
   app_key = "${var.datadog_app_key}"
